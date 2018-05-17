d76a0fe21b44ab1c2b1928cdacf73e40254c19ff
Cuneyt Tuna <cuneyt.tuna@gmail.com>
2018-05-17 11:14:41 -0400

    added environment variables from envFile configuration

---
+++ src/it/up-env/verify.groovy
@@ -0,0 +1,12 @@
+import java.nio.file.Paths
+
+String buildLog = new File("${basedir}/build.log").getText("UTF-8")
+
+String composeFile = Paths.get("${basedir}/docker-compose.yml").toString()
+
+assert buildLog.contains("Running: docker-compose -f $composeFile up --no-color" as CharSequence)
+assert buildLog.contains("Docker Compose has run successfully with message hello")
+
+def cleanUpProcess = new ProcessBuilder("docker", "system", "prune", "-a", "-f").start().waitFor()
+
+assert cleanUpProcess == 0
