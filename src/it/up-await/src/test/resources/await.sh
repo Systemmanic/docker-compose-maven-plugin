fc8f168cb3cdb97388445badaf64a8a52853a652
joachimbjorklund <joachimbjorklund@gmail.com>
2018-12-09 19:42:09 +0100

    add await feature (#43)

---
+++ src/it/up-await/src/test/resources/await.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Waiting..."
+exit 0
--- src/main/java/com/dkanejs/maven/plugins/docker/compose/DockerComposeUpMojo.java
+++ src/main/java/com/dkanejs/maven/plugins/docker/compose/DockerComposeUpMojo.java
@@ -4,6 +4,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -36,5 +37,45 @@ public class DockerComposeUpMojo extends AbstractDockerComposeMojo {
 		args.add("--no-color");
 
 		super.execute(args);
+
+		if (awaitCmd != null) {
+			await(buildCmd(awaitCmd, awaitCmdArgs));
+		}
+	}
+
+	private List<String> buildCmd(String cmd, String args) {
+		String[] cmdArgs = null;
+		if (args != null) {
+			cmdArgs = args.split(",");
+		}
+		List<String> answer = new ArrayList<>();
+		answer.add(cmd);
+		if (cmdArgs != null) {
+			answer.addAll(Arrays.asList(cmdArgs));
+		}
+		return answer;
+	}
+
+	private void await(List<String> awaitCmd) {
+		Integer exitCode = null;
+		ProcessBuilder pb = new ProcessBuilder(awaitCmd).inheritIO();
+		long start = System.currentTimeMillis();
+		try {
+			boolean stillWaiting;
+			do {
+				Process process = null;
+				if (exitCode != null) {
+					Thread.sleep(1000);
+				}
+				exitCode = pb.start().waitFor();
+				stillWaiting = (System.currentTimeMillis() - start) < (awaitTimeout * 1000);
+			} while ((exitCode != 0) && stillWaiting);
+
+			if (exitCode != 0) {
+				throw new RuntimeException("await failed after " + awaitTimeout + " seconds");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
